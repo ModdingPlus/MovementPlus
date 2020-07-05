dbeda41090473350a89075df596b8dd3e69592d1
Necro <necro@necro.dev>
2020-07-06 01:27:08 +0200

    fixed bug causing gamerules being registered multiple times

--- src/main/java/dev/necro/coyotelib/common/game_rules/GameRuleRegistrationHandler.java
+++ src/main/java/dev/necro/coyotelib/common/game_rules/GameRuleRegistrationHandler.java
@@ -3,22 +3,33 @@ package dev.necro.coyotelib.common.game_rules;
 import com.mojang.brigadier.CommandDispatcher;
 import dev.necro.coyotelib.CoyoteLib;
 import dev.necro.coyotelib.api.common.game_rules.RegisterGameRuleEvent;
+import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.impl.GameRuleCommand;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = CoyoteLib.MODID)
-public class GameRuleRegistrationHandler {
+public class GameRuleRegistrationHandler {    private static boolean registered = false;
 
     @SubscribeEvent
     public static void init(final FMLServerAboutToStartEvent event){
+        if(!GameRuleRegistrationHandler.registered) {
+            MinecraftForge.EVENT_BUS.post(new RegisterGameRuleEvent());
+            GameRuleRegistrationHandler.registered = true;
+        }
+
         CommandDispatcher<CommandSource> dispatcher = event.getServer().getCommandManager().getDispatcher();
-
-        MinecraftForge.EVENT_BUS.post(new RegisterGameRuleEvent());
-
         GameRuleCommand.register(dispatcher);
+    }
+
+    @SubscribeEvent
+    public static void init(final GuiOpenEvent event){
+        if(GameRuleRegistrationHandler.registered || !(event.getGui() instanceof MainMenuScreen)) return;
+        MinecraftForge.EVENT_BUS.post(new RegisterGameRuleEvent());
+        GameRuleRegistrationHandler.registered = true;
     }
 }
